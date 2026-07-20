12d8d151de77ea219d96b22df6f1172433a7d87f
魏曹先生 <1992414357@qq.com>
2026-07-20 12:07:48 +0800

    fix(doc): target doc generation at mingling workspace root
    Use `--manifest-path mingling/Cargo.toml` instead of `--workspace` to
    avoid generating documentation for unintended root workspace crates

--- .run/src/bin/doc.ps1
+++ .run/src/bin/doc.ps1
@@ -1,1 +1,5 @@
-cargo doc --workspace --no-deps --features core,macros,builds,structural_renderer,repl,comp,parser,picker,clap,extra_macros --open
+cargo doc `
+    --manifest-path mingling/Cargo.toml `
+    --no-deps `
+    --features core,macros,builds,structural_renderer,repl,comp,parser,picker,clap,extra_macros `
+    --open
--- .run/src/bin/doc.sh
+++ .run/src/bin/doc.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cargo doc --workspace --no-deps --features core,macros,builds,structural_renderer,repl,comp,parser,picker,clap,extra_macros --open
+cargo doc \
+  --manifest-path mingling/Cargo.toml \
+  --no-deps \
+  --features core,macros,builds,structural_renderer,repl,comp,parser,picker,clap,extra_macros \
+  --open
