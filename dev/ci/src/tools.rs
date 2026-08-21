e78e99b0dd07f1fc6c356c9e510ab5faf53a626a
魏曹先生 <1992414357@qq.com>
2026-08-22 05:24:58 +0800

    feat(ci): add refresh command to run all refresh tools

--- dev/ci/src/tools.rs
+++ dev/ci/src/tools.rs
@@ -1,3 +1,21 @@
+use mingling::macros::entry;
+use mingling::{ChainInvoker, macros::command};
+
 pub(crate) mod docsify_refresh;
 pub(crate) mod example_refresh;
 pub(crate) mod features_refresh;
+
+use crate::tools::docsify_refresh::EntryDocsifyRefresh;
+use crate::tools::example_refresh::EntryExampleRefresh;
+use crate::tools::features_refresh::EntryFeaturesRefresh;
+
+#[command]
+pub async fn refresh(
+    docsify: &ChainInvoker<EntryDocsifyRefresh>,
+    example: &ChainInvoker<EntryExampleRefresh>,
+    features: &ChainInvoker<EntryFeaturesRefresh>,
+) {
+    docsify.invoke_to_last(entry!()).await;
+    example.invoke_to_last(entry!()).await;
+    features.invoke_to_last(entry!()).await;
+}
